122933c9b06c3d45a6f3523a5e8c82b41f36a999
komandante999 <dreftware@yandex.ru>
2024-07-24 21:49:17 +0700

    menu

---
+++ system/storage/cache/template/12/1294b68e99d83b0b6d9195e30261047d.php
@@ -0,0 +1,55 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* multishop/template/common/menu.twig */
+class __TwigTemplate_e2d0919647bee5a6f35a545c1bf5b84c extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "MUNU
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "multishop/template/common/menu.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "multishop/template/common/menu.twig", "");
+    }
+}
---
+++ system/storage/cache/template/69/698ac804f76546b7c11bde7d06e7bb39.php
@@ -0,0 +1,70 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* multishop/template/common/menu.twig */
+class __TwigTemplate_954fca84fca7bf45b83f3c7728accc14 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<pre>
+";
+        // line 2
+        echo twig_var_dump($this->env, $context, ...[0 => ($context["categories"] ?? null)]);
+        echo "
+
+</pre>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "multishop/template/common/menu.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<pre>
+{{ dump(categories) }}
+
+</pre>
+", "multishop/template/common/menu.twig", "");
+    }
+}
